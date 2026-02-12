59f99474d170d3bcba153a9c9f8c5c8850ff4077
firojtasildar <firojtasildar117@gmail.com>
2026-02-12 12:47:32 +0530

    added run.bat file

---
+++ Run.bat
@@ -0,0 +1,2 @@
+pytest -v --html=Html_Reports\OrangeHRM_Login_crome.html --alluredir=Allure_Reports --browser chrome
+pytest -v --html=Html_Reports\OrangeHRM_Login_firefox.html --alluredir=Allure_Reports --browser=firefox
